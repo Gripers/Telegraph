aecb582653f574dc8c2f04733ac6f93184620aa9
Grip_Ers <87687774+Gripers@users.noreply.github.com>
2022-01-12 17:32:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function Telegrapher() {
+    var buton = document.getElementById("telegraph-btn");
+    buton.innerHTML = "Telegraph";
+    buton.classList.add("telegraphon");
+}
+
+function Requester() {
+    var baiton = document.getElementById("hokage");
+    baiton.innerHTML = "Request For Eary Access";
+    baiton.classList.add("requeston");
+}
